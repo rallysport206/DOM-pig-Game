30acfab9b2a6d16eb5de9b3cbdfc499f1bb9d1a8
rallysport206 <phan.tony125@gmail.com>
2020-04-20 22:13:06 -0700

    text content

--- app.js
+++ app.js
@@ -18,3 +18,9 @@ dice = Math.floor(Math.random() * 6) + 1;
 console.log(dice);
 
 document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
+
+let x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
