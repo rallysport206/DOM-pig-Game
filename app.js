2df8ae178b8d23c4d02e39769037b4bf84a4a3ab
rallysport206 <phan.tony125@gmail.com>
2020-04-21 20:09:05 -0700

    add click event listener, display results, and setup scores

--- app.js
+++ app.js
@@ -8,19 +8,40 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-let scores, roundScore, activePlayer, dice;
+let scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
-
-let x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
+
+// //callback function
+// function btn(){
+//     //Do something here
+// }
+// btn();
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //1. random number
+    let dice = Math.floor(Math.random() * 6) + 1;
+    
+    //2. display result
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3. update round score IF the rolled number was NOT a 1
+
+});
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
+
+// let x = document.querySelector('#score-0').textContent;
+// console.log(x);
